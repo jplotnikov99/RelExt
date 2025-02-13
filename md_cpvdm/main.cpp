f30d93ce95e71636be14b4cb0403bafad24c6a33
Johann Plotnikov <johann.plotnikov@online.de>
2025-02-13 16:19:03 +0100

    prepared example input files

--- md_cpvdm/main.cpp
+++ md_cpvdm/main.cpp
@@ -25,14 +25,13 @@ int main(int argc, char **argv) {
     M.set_channels(CONSIDERCHANNELS, NEGLECTCHANNELS, NEGLECTPARTICLES);
 
     clock_t begin_time = clock();
-    // M.InitMonteCarlo(100, 2000, 0, 0.12);
-    for (size_t i = 1; i <= 1; i++) {
-        M.LoadParameters(i);
-        M.RWalk(0.12, 0.002, 0.01, 300);
-        // M.SetWeight();
+    M.InitMonteCarlo(100, 500, 1, 0.12);
+    for (size_t i = 1; i <= 1e3; i++) {
+        M.LoadParameters();
+        M.CalcRelic();
+        M.SetWeight();
         M.SaveData(SAVEPARS);
     }
-
     std::cout << "Computation time:\n"
               << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
 }
